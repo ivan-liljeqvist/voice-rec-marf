e55fc3bf6afbcfb3f433b5726d5a52632a5247fa
ivan-liljeqvist <ivanli@kth.se>
2016-03-07 19:10:11 +0100

    better documentation

--- src/main/java/user/UserService.java
+++ src/main/java/user/UserService.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 /**
  * Created by IvanLiljeqvist on 07/03/16.
+ * This class handles all logic that has to do with User.
  */
 
 public class UserService {
@@ -24,13 +25,14 @@ public class UserService {
     private final MongoCollection<Document> collection;
 
     public UserService(MongoDatabase db) {
-
-
-
         this.db = db;
         this.collection = db.getCollection("user");
     }
 
+    /**
+     * Get all users in the DB.
+     * @return List of all users.
+     */
     public FindIterable<Document> findAll(){
 
         List<User> users = new ArrayList<>();
@@ -41,6 +43,10 @@ public class UserService {
 
     }
 
+    /**
+     * Takes a JSON as input and inserts it as a User in the DB.
+     * @param body
+     */
     public void createNewUser(String body) {
         User user = new Gson().fromJson(body, User.class);
 
@@ -53,12 +59,21 @@ public class UserService {
         collection.insertOne(document);
     }
 
+    /**
+     * Removes all users with a given first and last name.
+     * @param firstname
+     * @param lastname
+     */
     public void removeUsersWithName(String firstname, String lastname){
 
         db.getCollection("user").deleteMany(new Document("firstname", firstname).append("lastname",lastname));
 
     }
 
+    /**
+     * Finds a user with a specific id.
+     * @param id
+     */
     public void find(String id) {
         //return new User((BasicDBObject) collection.findOne(new BasicDBObject("_id", new ObjectId(id))));
     }
